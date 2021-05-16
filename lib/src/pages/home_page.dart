83ff4f2197668d20d0ebe8ff3698486fbd97aebe
stiven212 <stivfunk2000@hotmail.com>
2021-05-16 14:11:12 -0500

    Button added

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+  TextStyle estiloTexto = TextStyle(fontSize: 35);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,7 +12,24 @@ class HomePage extends StatelessWidget {
         elevation: 0.0,
       ),
       body: Center(
-        child: Text('Hola Mundo'),
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Numero de clicks',
+            style: estiloTexto,
+          ),
+          Text(
+            '0',
+            style: estiloTexto,
+          )
+        ],
+      )),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.cached),
+        onPressed: () {
+          print('Hola Mundo!');
+        },
       ),
     );
   }
