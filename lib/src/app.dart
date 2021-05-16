6b4dd11369fb6095e663536329e729439814e110
stiven212 <stivfunk2000@hotmail.com>
2021-05-16 12:52:28 -0500

    Hola Mundo con Scaffold

---
+++ lib/src/app.dart
@@ -0,0 +1,14 @@
+//import 'package:flutter_application_1/src/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+import 'pages/home_page.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Center(
+      child: HomePage(),
+    ));
+  }
+}
---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Titulo'),
+        centerTitle: true,
+        elevation: 0.0,
+      ),
+      body: Center(
+        child: Text('Hola Mundo'),
+      ),
+    );
+  }
+}
